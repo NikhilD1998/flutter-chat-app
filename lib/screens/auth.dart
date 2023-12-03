f58f637d50f74f5a97a8f67795c1d9eb5c34bd9a
NikhilD1998 <nikhildadab@hotmail.com>
2023-12-03 20:40:09 +0530

    Added Push Notification

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:chat_app/widgets/user_image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
@@ -38,6 +37,7 @@ class _AuthScreenState extends State<AuthScreen> {
         _isAuthenticating = true;
       });
       if (_isLogin) {
+        // ignore: unused_local_variable
         final userCredentials = await _firebase.signInWithEmailAndPassword(
             email: _enteredEmail, password: _enteredPassword);
       } else {
